b775a15899808d64090b46ec3aad3a03063cc2ec
Giovani Generali <giovani.generali@gmail.com>
2019-06-03 15:59:13 -0300

    deploy heroku

---
+++ public/precache-manifest.0dee6e87ef6268225e83cafa62bb7eec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b088d77614ad20eca4f05ad78aaaea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "211e3d269d81af5f947d",
+    "url": "/static/css/main.bf000caa.chunk.css"
+  },
+  {
+    "revision": "16bfd29d5c15ece0fa28",
+    "url": "/static/js/2.5ac9627d.chunk.js"
+  },
+  {
+    "revision": "211e3d269d81af5f947d",
+    "url": "/static/js/main.c977295d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ad279b698c4a3725dedd6619b5b2c552",
+    "url": "/static/media/spacex.ad279b69.png"
+  }
+]);
---
+++ schema.js
@@ -0,0 +1,68 @@
+const api = require("./api");
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLList,
+  GraphQLSchema
+} = require("graphql");
+
+// Launch Type
+const LaunchType = new GraphQLObjectType({
+  name: "Launch",
+  fields: () => ({
+    flight_number: { type: GraphQLInt },
+    mission_name: { type: GraphQLString },
+    launch_year: { type: GraphQLString },
+    launch_date_local: { type: GraphQLString },
+    launch_success: { type: GraphQLBoolean },
+    details: { type: GraphQLString },
+    links: { type: LaunchLinks },
+    rocket: { type: RocketType }
+  })
+});
+
+// Launch Links
+const LaunchLinks = new GraphQLObjectType({
+  name: "links",
+  fields: () => ({
+    mission_patch_small: { type: GraphQLString }
+  })
+});
+
+// Rocket Type
+const RocketType = new GraphQLObjectType({
+  name: "Rocket",
+  fields: () => ({
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString },
+    rocket_type: { type: GraphQLString }
+  })
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      resolve() {
+        return api.get("/launches").then(res => res.data);
+      }
+    },
+    launch: {
+      type: LaunchType,
+      args: {
+        flight_number: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        return api.get(`/launches/${args.flight_number}`).then(res => res.data);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
